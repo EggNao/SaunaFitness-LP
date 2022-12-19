b7d3daa8d317f151883927a8e574e15b0a676200
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-19 13:24:53 +0900

    tailwindの適用範囲変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
   plugins: [],
   theme: {
+    // colors: {
+    //   current: 'currentColor',
+    //   'orange': '#EC6A30',
+    //   'saunaBlue': '#4792F2',
+    //   'saunaGreen': '#65DB7C',
+    //   'saunaRed': '#EB4D3D',
+    //   transparent: 'transparent',
+    //   'yellow': '#E4E911'
+    // },
     extend: {},
   },
-};
+}
