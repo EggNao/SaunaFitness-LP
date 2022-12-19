4e00b4ee4e16430a42d25fc76259ba95b8eacf1a
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-19 19:56:59 +0900

    トップページ作成

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,12 +10,16 @@ export const Header: React.FC = () => {
   const headerIcon = '/headerIcon.svg'
   return (
     <header>
-      <nav className='border border-gray-200 px-4 py-2.5'>
-        <div className='flex max-w-screen-xl items-center justify-between'>
+      <nav className='border border-gray-200 py-2.5'>
+        <div className='m-auto flex max-w-screen-lg grid-cols-2 place-content-center items-center justify-between'>
           <Link href='/'>
             <Image src={headerIcon} className='mr-3' width={250} height={50} alt='Header Icon' />
           </Link>
-          <Button text={'Download'} color={'orange'}/>
+          <div className={'items-center'}>
+            <Link target="_blank" href='https://apps.apple.com/jp/app/%E3%82%B5%E3%82%A6%E3%83%8A%E3%82%BF%E3%82%A4%E3%83%A0/id6443695248'>
+              <Button text={'Download'} color={'orange'} />
+            </Link>
+          </div>
         </div>
       </nav>
     </header>
