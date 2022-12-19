6ed2a4733dc603fef8cd004de3dfa5d532c2a365
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-19 19:56:46 +0900

    ボタンコンポーネント修正

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -5,16 +5,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 type ButtonProps = {
   color: 'red' | 'orange' | 'blue'
-  rounded?: boolean
+  rounded?: boolean,
   text: string
 }
 
 export const Button: React.FC<ButtonProps> = ({ color, rounded=false, text }) => {
   // colorで表示色を変える
   const colorSrc = {
-    blue: 'border-transparent text-white bg-gradient-to-r from-green-400 to-blue-500 hover:bg-gradient-to-l',
-    orange: 'border-transparent text-white bg-gradient-to-r from-orange-500 to-yellow-400 hover:bg-gradient-to-l',
-    red: 'border-transparent text-white bg-gradient-to-r from-pink-500 to-yellow-500 hover:bg-gradient-to-l',
+    blue: 'border-transparent text-white bg-gradient-to-r from-green-400 to-blue-500 hover:bg-gradient-to-l hover:scale-110',
+    orange: 'border-transparent text-white bg-gradient-to-r from-orange-500 to-yellow-400 hover:bg-gradient-to-l hover:scale-110',
+    red: 'border-transparent text-white bg-gradient-to-r from-pink-500 to-yellow-500 hover:bg-gradient-to-l hover:scale-110',
   }
 
   // ボタンの大きさを変える
@@ -23,7 +23,7 @@ export const Button: React.FC<ButtonProps> = ({ color, rounded=false, text }) =>
       className={clsx(
         `px-4 py-2 text-base font-medium shadow-sm`,
         rounded ? 'rounded-full' : 'rounded-md',
-        colorSrc[color]
+        colorSrc[color],
       )}
     >
       {text}
