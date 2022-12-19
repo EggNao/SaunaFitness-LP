88cc4dff489b651a4601a9ba886606c57bda32e1
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-19 13:44:14 +0900

    Header作成

---
+++ src/components/header/index.tsx
@@ -0,0 +1,23 @@
+import { Inter } from '@next/font/google'
+import Image from 'next/Image'
+import Link from 'next/link'
+
+import { Button } from '~/components/button'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const Header: React.FC = () => {
+  const headerIcon = '/AppIcon.svg'
+  return (
+    <header>
+      <nav className='border border-gray-200 px-4 py-2.5'>
+        <div className='flex max-w-screen-xl items-center justify-between'>
+          <Link href='/'>
+            <Image src={headerIcon} className='mr-3' width={250} height={50} alt='Header Icon' />
+          </Link>
+          <Button text={'Download'} color={'orange'}/>
+        </div>
+      </nav>
+    </header>
+  )
+}
