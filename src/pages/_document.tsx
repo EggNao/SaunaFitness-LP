f7252e758c14415f4fdf243f4881da539670f41e
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-24 23:52:21 +0900

    メタ情報追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name='Sauna Time' content='Sauna Time' />
+        <meta property="og:title" content='Sauna Time' />
+        <meta property="og:image" content='/advertisementImage.png' />
+        <meta property="og:description" content='ととのいを科学する' />
         <link rel='icon' href='/appIcon.svg' />
       </Head>
       <body>
