9a0b61d6ad5c5e0773798b978c4b9e50c50d0593
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-24 21:24:23 +0900

    favicon設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name='Sauna Time' content='Sauna Time' />
+        <link rel='icon' href='/appIcon.svg' />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Inter } from '@next/font/google'
+import Head from 'next/head'
 
 import { Benefit } from '~/components/block/benefit'
 import { Body } from '~/components/block/body'
@@ -11,13 +12,19 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-      <Top />
-      <Body />
-      <Feature />
-      <Benefit />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Sauna Time</title>
+        <link rel='icon' href={process.env.FAVICON_URL} />
+      </Head>
+      <div>
+        <Header />
+        <Top />
+        <Body />
+        <Feature />
+        <Benefit />
+        <Footer />
+      </div>
+    </>
   )
 }
