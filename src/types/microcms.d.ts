5c0641b5b08df51232bfbe964fdcd546a51fd2e6
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-25 08:43:32 +0900

    newsの型定義

---
+++ src/types/microcms.d.ts
@@ -0,0 +1,17 @@
+export type NewsTypw = {
+        contents: [
+            {
+                id: string,
+                title: string,
+                createdAt: string,
+                publishedAt: string,
+                revisedAt: string,
+                updatedAt: string,
+                url: string
+            }
+        ],
+        limit: number,
+        offset: number,
+        totalCount: number
+
+}
