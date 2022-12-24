017fa857acc865f2ccb8c08066686bb74d3a8ee4
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-24 20:10:16 +0900

    articleBoxコンポーネント作成

---
+++ src/components/parts/articleBox/index.tsx
@@ -0,0 +1,23 @@
+import { Inter } from '@next/font/google'
+import Image from 'next/image'
+
+import { useBreakpoint } from '~/hooks/breakpoints'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export type ArticleBoxProps = {
+  title: string
+  description: string
+  img: string
+}
+
+export const ArticleBox: React.FC<ArticleBoxProps> = ({ title, description, img }) => {
+  const breakpoint = useBreakpoint()
+  return (
+    <div className='m-auto w-64'>
+      <Image className={'m-auto mb-6'} height={200} width={200} src={img} alt='' />
+      <div className={'mb-4 text-center text-2xl'}>{title}</div>
+      <div>{description}</div>
+    </div>
+  )
+}
