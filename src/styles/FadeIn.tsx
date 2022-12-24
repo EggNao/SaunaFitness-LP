b7f85088b653368dd9f9974e91d9730e43abb87e
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-24 14:05:15 +0900

    スクロールアニメーションの関数実装

---
+++ src/styles/FadeIn.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+export type FadeInProps = {
+    children: ReactNode
+}
+
+export const FadeIn: React.FC<FadeInProps> = ({ children }) => {
+    const { inView, ref } = useInView({
+        // オプション
+        rootMargin: '-50px', // ref要素が現れてから50px過ぎたら
+        triggerOnce: true, // 最初の一度だけ実行
+    });
+
+    return (
+        <div
+            ref={ref}
+            className={`${inView ? "opacity-100" : "translate-y-[50%] opacity-0"} duration-[1s]`}
+        >
+            {children}
+        </div>
+    )
+}
