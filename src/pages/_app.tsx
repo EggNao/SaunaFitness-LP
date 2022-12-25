861819b918824fa923a2a581bbc83580fdc16d00
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-12-25 10:27:07 +0900

    google analytics導入 https://zenn.dev/rh820/articles/8af90011c573fe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,28 @@
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { useEffect } from 'react'
 
+import * as gtag from '~/lib/gtag'
+
+export const GAID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_IDD ?? ''
+
 export default function App({ Component, pageProps }: AppProps) {
+  // GAの関数
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouterChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouterChange)
+    }
+  }, [router.events])
+
+  // Threejsの関数
   useEffect(() => {
     const threeScript = document.createElement('script')
     threeScript.setAttribute('id', 'threeScript')
@@ -15,5 +34,23 @@ export default function App({ Component, pageProps }: AppProps) {
       }
     }
   }, [])
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${GAID}`} />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${GAID}');
+           `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
